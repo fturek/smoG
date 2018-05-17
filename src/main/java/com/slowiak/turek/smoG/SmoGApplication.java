6537ae8b44271d012463103ba34013159142501a
Filip Turek <filippturek@gmail.com>
2018-05-17 16:58:26 +0200

    init

---
+++ src/main/java/com/slowiak/turek/smoG/SmoGApplication.java
@@ -0,0 +1,12 @@
+package com.slowiak.turek.smoG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoGApplication.class, args);
+	}
+}
