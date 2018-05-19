7b890067bd30d2120523d7a527d0d7262ca25ae5
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-19 16:37:23 +0200

    Working database loader

--- src/main/java/com/slowiak/turek/smoG/SmoGApplication.java
+++ src/main/java/com/slowiak/turek/smoG/SmoGApplication.java
@@ -1,17 +1,28 @@
 package com.slowiak.turek.smoG;
 
+import com.slowiak.turek.smoG.database.StationDatabase;
+import com.slowiak.turek.smoG.model.Station;
+import com.slowiak.turek.smoG.repository.StationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SmoGApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmoGApplication.class, args);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(SmoGApplication.class, args);
 
+//        Wypisanie istniejacych beanow rozpoznanych przez springa
+//        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+//        Arrays.stream(beanDefinitionNames).forEach(System.out::println);
 
-//		StationDatabase stationDatabase = new StationDatabase();
-//		List<Station> stationsFromDatabase = stationDatabase.getStationsFromDatabase();
-//		stationsFromDatabase.forEach(station -> System.out.println(station.getStationName()));
-	}
+        StationDatabase stationDatabase = (StationDatabase) ctx.getBean("stationDatabase");
+        List<Station> all = stationDatabase.getStationsFromDatabase();
+        all.forEach(System.out::println);
+    }
 }
--- src/main/java/com/slowiak/turek/smoG/database/StationDatabase.java
+++ src/main/java/com/slowiak/turek/smoG/database/StationDatabase.java
@@ -2,18 +2,12 @@ package com.slowiak.turek.smoG.database;
 
 import com.slowiak.turek.smoG.model.Station;
 import com.slowiak.turek.smoG.repository.StationsRepository;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping
+@Controller
 public class StationDatabase {
 
     private StationsRepository stationsRepository;
@@ -23,9 +17,7 @@ public class StationDatabase {
         this.stationsRepository = stationsRepository;
     }
 
-    @GetMapping("/test")
-    public List<Station> getStationsFromDatabase(){
-        System.out.println(stationsRepository);
+    public List<Station> getStationsFromDatabase() {
         return stationsRepository.findAll();
     }
 }
