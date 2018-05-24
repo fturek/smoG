68636957831847d4b8cee5c4770b61ed95d5c7a0
Filip Turek <filippturek@gmail.com>
2018-05-24 17:35:18 +0200

    Add service to get all stations from url

---
+++ src/main/java/com/slowiak/turek/smoG/constant/URLConstants.java
@@ -0,0 +1,6 @@
+package com.slowiak.turek.smoG.constant;
+
+public class URLConstants {
+
+    public static String allStations = "http://api.gios.gov.pl/pjp-api/rest/station/findAll";
+}
---
+++ src/main/java/com/slowiak/turek/smoG/service/StationService.java
@@ -0,0 +1,32 @@
+package com.slowiak.turek.smoG.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.slowiak.turek.smoG.constant.URLConstants;
+import com.slowiak.turek.smoG.model.Station;
+import com.slowiak.turek.smoG.utils.JSONReader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class StationService {
+
+    public List<Station> getStationsFromAPI(){
+        JSONReader jsonReader = new JSONReader();
+        List<Station> stationList = null;
+        try {
+            String jsonText = jsonReader.readJsonFromUrl(URLConstants.allStations);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            stationList = objectMapper.readValue(jsonText, new TypeReference<List<Station>>() {
+            });
+        } catch (IOException e) {
+            // TODO Log handled error
+            e.printStackTrace();
+        }
+
+        return stationList;
+    };
+}
