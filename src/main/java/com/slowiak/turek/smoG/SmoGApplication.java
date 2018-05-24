59050b603c85df3cd377e7606f3433dd63da81f8
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-24 20:51:02 +0200

    Fetching data from API to database working

--- src/main/java/com/slowiak/turek/smoG/SmoGApplication.java
+++ src/main/java/com/slowiak/turek/smoG/SmoGApplication.java
@@ -1,16 +1,13 @@
 package com.slowiak.turek.smoG;
 
 import com.slowiak.turek.smoG.constant.Constants;
-import com.slowiak.turek.smoG.database.StationDatabase;
 import com.slowiak.turek.smoG.model.Station;
 import com.slowiak.turek.smoG.repository.StationsRepository;
 import com.slowiak.turek.smoG.service.StationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -22,23 +19,23 @@ public class SmoGApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(SmoGApplication.class, args);
 
-//        Wypisanie istniejacych beanow rozpoznanych przez springa
-//        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-//        Arrays.stream(beanDefinitionNames).forEach(System.out::println);
+        /*
+        StationService stationService = (StationService) ctx.getBean("stationService");
+        StationsRepository stationsRepository = (StationsRepository) ctx.getBean("stationsRepository");
+        List<Station> stationsFromAPI = stationService.getStationsFromAPI();
+        stationsRepository.saveAll(stationsFromAPI);
+        */
 
-//        StationDatabase stationDatabase = (StationDatabase) ctx.getBean("stationDatabase");
-//        List<Station> all = stationDatabase.getStationsFromDatabase();
-//        all.forEach(System.out::println);
 
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(Constants.threadAmmount);
         scheduledExecutorService.schedule(() -> {
+            System.out.println("schedule");
             StationService stationService = (StationService) ctx.getBean("stationService");
+            StationsRepository stationsRepository = (StationsRepository) ctx.getBean("stationsRepository");
             List<Station> stationsFromAPIList = stationService.getStationsFromAPI();
-
-            stationsFromAPIList.forEach(station -> {
-                System.out.println(station.getStationName()+"\n");
-            });
-
-        }, Constants.allStationsDelayDays, TimeUnit.DAYS);
+            System.out.println("start");
+            stationsRepository.saveAll(stationsFromAPIList);
+            System.out.println("stop");
+    },10, TimeUnit.SECONDS);
     }
 }
