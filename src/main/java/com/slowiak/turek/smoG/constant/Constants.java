196ebc396fb9ef5505fa6fd08afba38bc880ac64
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-26 18:59:50 +0200

    Url for sensors

--- src/main/java/com/slowiak/turek/smoG/constant/Constants.java
+++ src/main/java/com/slowiak/turek/smoG/constant/Constants.java
@@ -3,6 +3,7 @@ package com.slowiak.turek.smoG.constant;
 public class Constants {
 
     public static String allStationsURL= "http://api.gios.gov.pl/pjp-api/rest/station/findAll";
+    public static String sensorsWithStationIdURL= "http://api.gios.gov.pl/pjp-api/rest/station/sensors/";
     public static long allStationsDelayDays = 0;
     public static long allStationsPeriodDays = 1;
     public static int threadAmmount = 5;
