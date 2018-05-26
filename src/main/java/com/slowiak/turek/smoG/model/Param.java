4127afc40c4343003049ed70978ac7af4e606300
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-26 19:01:31 +0200

    JsonIgnoreProperties for Param mapping

--- src/main/java/com/slowiak/turek/smoG/model/Param.java
+++ src/main/java/com/slowiak/turek/smoG/model/Param.java
@@ -1,5 +1,6 @@
 package com.slowiak.turek.smoG.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -7,6 +8,7 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "params")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Param {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
